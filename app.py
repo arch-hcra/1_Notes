3eca930b8722a3c0909b5aaf99a51a981777111a
archhcra <bomcom@bk.ru>
2025-05-02 19:39:55 +0300

    2_check_docker

--- app.py
+++ app.py
@@ -31,4 +31,5 @@ def view_records():
     return render_template('records.html', records=records)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
